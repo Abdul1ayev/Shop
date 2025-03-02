d62b8edbe7b2fc665de824cc18b3fdff611c4ccd
Abdul1ayev <abdullayevsamir2009@gmail.com>
2025-03-02 13:19:19 +0500

    newPages

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { createClient } from "@/supabase/client";
 import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
 
 type CartItem = {
   id: string;
